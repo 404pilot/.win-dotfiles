9cb72570cf133e4f57538c8267be77ffe7842510
404pilot <404pilot@users.noreply.github.com>
2021-12-30 10:19:31 -0600

    update

--- up.ps1
+++ up.ps1
@@ -16,6 +16,11 @@ function Link_File
     New-Item -ItemType SymbolicLink -Path $Output -Target $Source
 }
 
+Write-Host "Creating necessary symbo links ..."
 Link_File -Source ".\git\gitconfig" -Output "~\.gitconfig"
 Link_File -Source ".\editor_config\editorconfig" -Output "~\.editorconfig"
 Link_File -Source ".\ssh\config" -Output "~\.ssh\config"
+Link_File -Source ".\vim\vimrc" -Output "~\.vimrc"
+
+Write-Host "Configuring cmd ..."
+REG import .\cmd\autorun_cmd_alias.reg
