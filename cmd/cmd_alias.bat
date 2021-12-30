814420287dd59bd357e429f70289fdc89c439518
404pilot <404pilot@users.noreply.github.com>
2021-12-30 13:47:50 -0600

    configure aliases for cmd

--- cmd/cmd_alias.bat
+++ cmd/cmd_alias.bat
@@ -1,11 +1,11 @@
 REM git
-@doskey gbye='git add --all; git reset --hard HEAD'
-@doskey gs='git status'
-@doskey gb='git branch'
-@doskey gma='git commit -am' $*
-@doskey gmm='git commit -m' $*
-@doskey gc='git checkout' $*
-@doskey gcm='git checkout master'
-@doskey ga='git add'
-@doskey gall='git add --all'
-@doskey gp='git pull'
+@doskey gbye=git add --all; git reset --hard HEAD
+@doskey gs=git status
+@doskey gb=git branch
+@doskey gma=git commit -am $*
+@doskey gmm=git commit -m $*
+@doskey gc=git checkout $*
+@doskey gcm=git checkout master
+@doskey ga=git add
+@doskey gall=git add --all
+@doskey gp=git pull
